1ca0aba58292d35f06c41dfb4efcbc08d4f6fd09
artofnext <novnovalex@gmail.com>
2019-11-06 17:25:00 +0200

    Fractions in progress

--- js/hw.js
+++ js/hw.js
@@ -46,67 +46,133 @@ function startShow() {
 
 //Fractions
 
-class Fraction {
-    constructor(n, d, w = 0) {
-        this._numerator = n + w * d;
-        this.denominator =d;
-    }
+// class Fraction {
+//     constructor(n, d, w = 0) {
+//         this._numerator = n + w * d;
+//         this.denominator =d;
+//     }
 
-    whole() {
-        return Math.floor(this._numerator / this.denominator);
-    }
+//     whole() {
+//         return Math.floor(this._numerator / this.denominator);
+//     }
 
-    numerator() {
-        return this._numerator % this.denominator;
+//     numerator() {
+//         return this._numerator % this.denominator;
+//     }
+// }
+
+// let whole = function() {
+//     return Math.floor(this._numerator / this.denominator);
+// };
+
+// let numerator = function() {
+//     return this._numerator % this.denominator;
+// };
+
+// let fr1 = {
+//     whole: function() {
+//         return Math.floor(this._numerator / this.denominator);
+//     },
+//     numerator: function() {
+//         return this._numerator % this.denominator;
+//     },
+//     _numerator: 2,
+//     denominator: 3,
+// };
+
+// let fr2 = {
+//     whole: function() {
+//         return Math.floor(this._numerator / this.denominator);
+//     },
+//     numerator: function() {
+//         return this._numerator % this.denominator;
+//     },
+//     _numerator: 8,
+//     denominator: 4,
+// };
+
+//Fractions init
+
+let fr1 = newFraction(23, 23);
+
+let fr2 = newFraction(4, 2);
+
+let fr3 = 2;
+
+
+function showFraction(a) {
+    if (a.numerator()) {
+        console.log(`${a.whole()} ${a.numerator()}/${a.denominator}`);
+    } else {
+        console.log(`${a.whole()}`);
     }
+};
+
+function isInteger(a) { //ToDo
+    return (typeof(a == 'number'));
 }
 
-let fr1 = {
-    whole: function() {
-        return Math.floor(a.numerator / a.denominator);
-    },
-    numerator: function() {
+function toFraction(a) {
+    let str = a.toString();
+    let factor = Math.pow(10, (str.length - str.indexOf('.')-1)); // Find the factor to get integer numerator
 
-    }
-    _numerator: 2,
-    denominator: 3,
+console.log(a * factor);
 
+    let c = newFraction(a, 1);
 }
 
-let fr2 = {
-    whole: 1,
-    numerator: 8,
-    denominator: 4,
+toFraction(12);
+
+function isFraction(a) {
+    return (isObject(a) && "denominator" in a && "_numerator" in a && a.denominator > 0);
 }
+
+console.log(isFraction(fr1));
+console.log(isFraction(fr3));
+
 
 function reduction(a) {
-    let reducer = gcd (a.numerator, a.denominator);
-    a.numerator = a.numerator / reducer;
+    let reducer = gcd (a._numerator, a.denominator);
+    a._numerator = a._numerator / reducer;
     a.denominator = a.denominator / reducer;
     return a;
-}
+};
 
-function normalise(a) {
-    if (a.numerator < a.denominator) {
-        return a;
-    }
-    a.whole = a.whole + Math.floor(a.numerator / a.denominator);
-    a.numerator = a.numerator % a.denominator;
-
-    if (a.numerator == 0) { //We have integer
-        a.denominator = 1;
-    }
-
-    return a;
+function newFraction(a, b) {
+    return {
+        whole: function() {
+            return Math.floor((this._numerator - this.numerator()) / this.denominator); //Workout negative numbers
+        },
+        numerator:  function() {
+            return this._numerator % this.denominator;
+        },
+        _numerator: a,
+        denominator: b,
+    };
 }
 
 function multiply(a, b) {
-    let c = {};
-    c.whole = 0;
-    c.numerator = (a.numerator + a.whole * a.denominator) * (b.numerator + b.whole * b.denominator);
+    let c = newFraction();
+    // c.whole = 0;
+    c._numerator = a._numerator * b._numerator;
     c.denominator = a.denominator * b.denominator;
 
-    return normalise(c);
+    return reduction(c);
+};
+
+function add(a, b) {
+    let c = newFraction();
+    let lcmAB = lcm (a.denominator, b.denominator);
+    let factorA = lcmAB / a.denominator;
+    let factorB = lcmAB / b.denominator;
+    c._numerator = a._numerator * factorA + b._numerator * factorB;
+    c.denominator = lcmAB;
+    return reduction(c);
+}
+
+function substract(a, b) {
+    b._numerator = b._numerator * -1;
+    return reduction(add(a, b))
 }
 
 console.log('Origin a:');
@@ -115,7 +181,14 @@ console.log('Origin b:');
 console.table(fr2);
 
 console.log('Multiplyed a * b:');
-console.table(multiply(fr1, fr2));
+// console.table(multiply(fr1, fr2));
+showFraction(multiply(fr1, fr2));
+console.log('Add a + b:');
+showFraction(add(fr1, fr2));
+console.log('Substract a - b:');
+showFraction(substract(fr1, fr2));
+// console.table(substract(fr1, fr2));
+// showFraction(newFraction());
 
 // console.log('Reduced a:');
 // console.table(reduction(fr1));
