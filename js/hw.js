e40e62d99cd3848e323e08dcbb43458754c9a1d6
artofnext <novnovalex@gmail.com>
2019-11-06 11:50:54 +0200

    car object

--- js/hw.js
+++ js/hw.js
@@ -1,5 +1,5 @@
 // import { isNull } from "util";
-
+//Car Object
 let car = {
     manufacturer: "handmade",
     model: "unic",
@@ -12,10 +12,13 @@ let car = {
 
 console.log(typeof(car));
 
+function isObject(obj) {
+    return (typeof(obj) == 'object' && !!obj);
+}
+
 function showObject(obj) {
 
-    if (typeof(obj) == 'object' && !!obj) {
-
+    if (isObject(obj)) {
         for (let key in obj) {
             console.log(`Property: ${key} - Value: ${obj[key]}`);
         }
@@ -24,5 +27,33 @@ function showObject(obj) {
     }
 };
 
-showObject(car);
+function showObjectAlt(obj) {
+    if (isObject(obj)) {
+        console.table(obj);
+    } else {
+        console.log(`Wrong function param!`);
+    }
+};
 
+function startShow() {
+
+    console.log('Show With For');
+    showObject(car);
+    
+    console.log('Show With Table');
+    showObjectAlt(car);
+}
+
+//Fractions
+
+let fr1 = {
+    whole: 0,
+    numerator: 2,
+    denominator: 3,
+}
+
+let fr2 = {
+    whole: 1,
+    numerator: 6,
+    denominator: 4,
+}
